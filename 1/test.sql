403e3d65a3a289454e806bbce58818154c41131b
lrfuller <33261077+lrfuller@users.noreply.github.com>
2021-07-28 09:32:53 -0400

    1

--- 1/test.sql
+++ 1/test.sql
@@ -25,8 +25,11 @@ WHERE cpsys_DataCurrent.Dataid = cprel_News.DataId
             SELECT cpsys_DataCurrentInTaxonomy.DataId
             FROM cpsys_DataCurrentInTaxonomy
             WHERE (
-                    cpsys_DataCurrentInTaxonomy.TaxonomyId IN ('1b16e72b-d8b3-47d3-9eaa-56be5a08061f')
+                    cpsys_DataCurrentInTaxonomy.TaxonomyId IN ('fc062792-2768-460f-ae05-03b10da1a8d0')
                 )
+                --             WHERE (
+                --     cpsys_DataCurrentInTaxonomy.TaxonomyId IN ('1b16e72b-d8b3-47d3-9eaa-56be5a08061f')
+                -- )
         )
     )
     AND (cprel_News.IsEnabled = 1)
